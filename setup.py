7a95554f32d1e7abcce5663ce79e563c398acc69
vasnake <vasnake@gmail.com>
2014-01-17 19:46:01 +0400

    setup.py writed

--- setup.py
+++ setup.py
@@ -3,27 +3,37 @@
 # Copyright (c) Valentin Fedulov <vasnake@gmail.com>
 # See COPYING for details.
 
-from distutils.core import setup
-import enc
+from setuptools import setup
 
-long_description = open('README.rst', 'r').read()
-description = 'Russian transliteration module'
+with open('README.rst', 'r') as infile:
+    long_description = infile.read()
 
 setup(
-        name='enc',
-        version=enc.__version__,
-        description=description,
-        long_description=long_description,
-        author=enc.__author__,
-        author_email='vasnake@gmail.com',
-        url='https://github.com/vasnake/transbot',
-        license='GPL',
-        classifiers=[
-                'Development Status :: 4 - Beta',
-                'Intended Audience :: Developers',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python',
-                'Programming Language :: Python :: 2.7',
-        ],
-        py_modules=['enc'],
+    name = 'translitbot',
+    description = "Translit XMPP chat bot",
+    long_description = long_description,
+    keywords = "XMPP GTalk chat bot translit",
+    url = 'https://github.com/vasnake/transbot',
+    download_url = 'https://github.com/vasnake/transbot/archive/master.zip',
+    version = "0.1.2",
+    license = 'GPLv3',
+    author = "Valentin Fedulov",
+    author_email='vasnake@gmail.com',
+    packages = ['translitbot'],
+    scripts = [],
+    install_requires = ['dnspython', 'trans', 'xmpppy'],
+    classifiers = [ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: Freeware',
+        'Topic :: Communications :: Chat',
+        'Topic :: Utilities'
+    ],
+    zip_safe = False
 )
